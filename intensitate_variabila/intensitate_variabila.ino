a6254f419c382ae33ef97287c43f6e521e701d18
Andrei Dore <andrei.dore@gmail.com>
2019-05-07 10:01:06 +0300

    Format code.

--- intensitate_variabila/intensitate_variabila.ino
+++ intensitate_variabila/intensitate_variabila.ino
@@ -1,13 +1,13 @@
 void setup() {
-	pinMode(11, OUTPUT);
+  pinMode(11, OUTPUT);
 }
 void loop() {
-	for (int i = 0; i < 255; i++){
-		analogWrite(11, i);
-		delay(50);
-	}
-	for (int i = 255; i > 0; i--){
-		analogWrite(11, i);
-		delay(50);
-	}
+  for (int i = 0; i < 255; i++) {
+    analogWrite(11, i);
+    delay(50);
+  }
+  for (int i = 255; i > 0; i--) {
+    analogWrite(11, i);
+    delay(50);
+  }
 }
